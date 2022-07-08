5d3c0839a2c763f11daf1330ad32f278fc246fac
thecode00 <thecode0087@gmail.com>
2022-07-08 13:48:18 +0900

    Leetcode problem solve

--- Leetcode/Python3/724. Find Pivot Index/solution.py
+++ Leetcode/Python3/724. Find Pivot Index/solution.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/find-pivot-index/
 
 # solution from here: https://leetcode.com/problems/find-pivot-index/discuss/109255/Short-Python-O(n)-time-O(1)-space-with-Explanation
-class Solution:
+class Solution:  # Runtime: 169 ms
     def pivotIndex(self, nums: List[int]) -> int:
         # If pivot index == 0 or -1 left_sum or right_sum will be 0
         left, right = 0, sum(nums)
