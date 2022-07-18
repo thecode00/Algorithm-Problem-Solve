87b63df4238e40c8f85fbb2332865db771131c6b
thecode00 <thecode0087@gmail.com>
2022-07-18 14:51:12 +0900

    Leetcode problem solve

--- Leetcode/Python3/876. Middle of the Linked List/solution.py
+++ Leetcode/Python3/876. Middle of the Linked List/solution.py
@@ -5,6 +5,13 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# https://leetcode.com/problems/middle-of-the-linked-list/discuss/1651600/PythonJavaC%2B%2B-Simple-Solution-oror-One-Pass-oror-Beginner-Friendly-oror-Detailed-Explanation
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return head[len(head) // 2:]
+        slow = fast = head
+        while fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+            
