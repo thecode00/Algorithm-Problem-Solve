89d153b8ae5e39b441fd62a099228a34d2c358da
thecode00 <thecode0087@gmail.com>
2022-07-24 20:29:35 +0900

    Baekjoon problem solve

--- Baekjoon/C++/11654. 아스키 코드/solution.cpp
+++ Baekjoon/C++/11654. 아스키 코드/solution.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     char a;
-
     scanf("%c", &a);
     printf("%d", a);
 }
---
+++ Baekjoon/C++/2743. 단어 길이 재기/solution.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/2743
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string s;
+    getline(cin, s);
+    cout << s.length();
+}
