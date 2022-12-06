0cc2a6551f2d744e5fe5069e9aaa25e27e3ad656
thecode00 <thecode0087@gmail.com>
2022-12-06 22:56:40 +0900

    Baekjoon problem solve

--- Baekjoon/Python3/2293. 동전 1/solution.py
+++ Baekjoon/Python3/2293. 동전 1/solution.py
@@ -10,14 +10,15 @@ dp = [0 for _ in range(k + 1)]
 dp[0] = 1
 coins = [int(input()) for _ in range(n)]
 
-for idx in range(1, k + 1):
-    for c in coins:
-        if idx - c > 0:
-            dp[idx] += dp[idx - c]
-
-# for c in coins:
-#     for idx in range(1, k + 1):
-#         if idx - c >= 0:
+# for idx in range(1, k + 1):
+#     for c in coins:
+#         if idx - c > 0:
 #             dp[idx] += dp[idx - c]
+
+# 코인가치 c의 배수마다 조합수가 1씩 늘어나는것을 이용
+for c in coins:
+    for idx in range(1, k + 1):
+        if idx - c >= 0:
+            dp[idx] += dp[idx - c]
+    print(dp)
 print(dp[k])
-print(dp)
