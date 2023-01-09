b48292d86daca7a290e39e1b211b585742313b18
thecode00 <thecode0087@gmail.com>
2023-01-10 05:49:12 +0900

    Leetcode problem solve

--- Leetcode/C++/1. Two Sum/solution.cpp
+++ Leetcode/C++/1. Two Sum/solution.cpp
@@ -1,27 +1,30 @@
 // https://leetcode.com/problems/two-sum/
 #include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         unordered_map<int, int> numMap;
         vector<int> answer;
         for (int i = 0; i < nums.size(); i++)
         {
             if (numMap.find(nums[i]) == numMap.end())
             {
-                numMap.insert({target - nums[i], i});
-            } 
-            else {    // Number found
+                numMap.insert({ target - nums[i], i });
+            }
+            else
+            { // Number found
                 answer.push_back(numMap[nums[i]]);
                 answer.push_back(i);
                 return answer;
             }
         }
-        return answer;  // Number not found
+        return answer; // Number not found
     }
 };
