4e5194fbef9a5ccc5b40bd4a709c90a6ede32f56
thecode00 <thecode0087@gmail.com>
2023-01-11 00:22:24 +0900

    Leetcode problem solve

---
+++ Leetcode/C++/1920. Build Array from Permutation/solution.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        vector<int> answer;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            answer.push_back(nums[nums[i]]);
+        }
+
+        return answer;
+    }
+};
---
+++ Leetcode/C++/1929. Concatenation of Array/solution.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/concatenation-of-array/
+
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int s = nums.size();
+        for (int i = 0; i < s; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
