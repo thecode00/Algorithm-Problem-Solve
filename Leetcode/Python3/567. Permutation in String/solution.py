04af93b8ce26fa853910f5f48501edded54fad9b
thecode00 <thecode0087@gmail.com>
2023-02-04 23:18:17 +0900

    Leetcode problem solve

--- Leetcode/Python3/567. Permutation in String/solution.py
+++ Leetcode/Python3/567. Permutation in String/solution.py
@@ -1,8 +1,28 @@
 # https://leetcode.com/problems/permutation-in-string/
 
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        c1 = Counter(s1)
+        cur_count = Counter()
+
+        left = right = 0
+        for right in range(len(s2)):
+            cur_count[s2[right]] += 1
+
+            if cur_count == c1:
+                return True
+            
+            if right >= len(s1) - 1:    # If len(window) == len(s1) move left index of window
+                cur_count[s2[left]] -= 1
+                left += 1
+                # If you add an empty counter, elements with a value of 0 disappear.
+                cur_count += Counter()
+        return False
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         len1 = len(s1)
