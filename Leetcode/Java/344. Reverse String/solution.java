5af5477d2377c1df5b45ea7ecfd0ef70a3047a20
thecode00 <thecode0087@gmail.com>
2024-06-20 18:00:28 +0900

    Solve leetcode problem

---
+++ Leetcode/Java/344. Reverse String/solution.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/reverse-string/description/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left += 1;
+            right -= 1;
+        }
+    }
+}
