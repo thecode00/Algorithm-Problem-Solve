3459997ec4cf13e4f37377f377dd5899938df62b
thecode00 <thecode0087@gmail.com>
2025-01-28 00:36:33 +0900

    Solve leetcode problem

---
+++ Leetcode/Python/1462. Course Schedule IV/solution.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/course-schedule-iv/description
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        in_degree = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
+        # is_reachable[a][b] = b can reach a
+        is_reachable = [[False] * numCourses for _ in range(numCourses)]
+
+        for pre, to in prerequisites:
+            in_degree[to] += 1
+            graph[pre].append(to)
+
+        queue = deque()
+        for idx in range(numCourses):
+            if in_degree[idx] == 0:
+                queue.append(idx)
+                is_reachable[idx][idx] = True
+
+        while queue:
+            cur = queue.popleft()
+
+            for course in graph[cur]:
+                in_degree[course] -= 1
+                for idx in range(numCourses):
+                    is_reachable[course][idx] |= is_reachable[cur][idx]
+                is_reachable[course][course] = True
+
+                if in_degree[course] == 0:
+                    queue.append(course)
+
+        result = []
+        for query in queries:
+            result.append(is_reachable[query[1]][query[0]])
+
+        return result
